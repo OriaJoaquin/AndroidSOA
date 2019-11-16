56d0068d2ce729ce352dda82235b358b8d1630b4
Joaquín Oria <joaquin.ariel.o@gmail.com>
2019-11-16 18:25:07 -0300

    se agrego clase servicio de bluetooth

--- app/src/main/java/com/android/brillame/MainActivity.java
+++ app/src/main/java/com/android/brillame/MainActivity.java
@@ -6,10 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "LEDv0";
+    public static final boolean D = true;
+    // Tipos de mensaje enviados y recibidos desde el Handler de ConexionBT
+    public static final int Mensaje_Estado_Cambiado = 1;
+    public static final int Mensaje_Leido = 2;
+    public static final int Mensaje_Escrito = 3;
+    public static final int Mensaje_Nombre_Dispositivo = 4;
+    public static final int Mensaje_TOAST = 5;
+    public static final int MESSAGE_Desconectado = 6;
+    public static final int REQUEST_ENABLE_BT = 7;
 
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+    //Nombre del dispositivo conectado
+    private String mConnectedDeviceName = null;
+    // Adaptador local Bluetooth
+    private BluetoothAdapter AdaptadorBT = null;
+    //Objeto miembro para el servicio de ConexionBT
+    private ConexionBT Servicio_BT = null;
+    //Vibrador
+    private Vibrator vibrador;
+    //variables para el Menu de conexión
+    private boolean seleccionador = false;
     Button btnAbrirActivityAnalizarObjeto;
     Button btnAbrirActivityBuscarDispositivos;
     Button btnAbrirActivityEstadisticas;
@@ -50,22 +88,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void abrirActivityBuscarDispositivos(View view){
+    public void abrirActivityBuscarDispositivos(View view) {
         //Intent intent = new Intent(this, );77
         Intent intent = new Intent(this, BuscarDispositivosActivity.class);
         startActivity(intent);
     }
 
-    public void abrirActivityAnalizarObjeto(View view){
+    public void abrirActivityAnalizarObjeto(View view) {
         Intent intent = new Intent(this, AnalizarObjetoActivity.class);
         startActivity(intent);
     }
 
-    public void abrirActivityEstadistica(View view){
+    public void abrirActivityEstadistica(View view) {
         Intent intent = new Intent(this, EstadisticasActivity.class);
         startActivity(intent);
     }
-
 
 
 }
