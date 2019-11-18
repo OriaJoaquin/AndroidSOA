d352e7e67eee603b4f36ea37592f403777a6ede2
Ernesto Facal <facerne123@gmail.com>
2019-11-18 18:21:20 -0300

    sensor de proximidad agregado

--- app/src/main/java/com/android/brillame/PrenderApagarLucesActivity.java
+++ app/src/main/java/com/android/brillame/PrenderApagarLucesActivity.java
@@ -2,13 +2,71 @@ package com.android.brillame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class PrenderApagarLucesActivity extends AppCompatActivity {
+    private SensorManager mSensorManager;
+    private Sensor mProximity;
+    private static final int SENSOR_SENSITIVITY = 4;
+    ImageView imagenFoco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prender_apagar_luces);
+        imagenFoco = this.findViewById(R.id.imageViewFoco);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.registerListener(sensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY), SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+      //  mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+      //  mSensorManager.unregisterListener(this);
+    }
+
+
+
+
+
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                Log.d("pruebasensor",String.valueOf(event.values[0]));
+                Log.d("pruebasensor",String.valueOf(SENSOR_SENSITIVITY));
+              //  if ((event.values[0] >= -SENSOR_SENSITIVITY) && (event.values[0] <= SENSOR_SENSITIVITY)) {
+                if (event.values[0] == 0) {
+                    //near
+                    Toast.makeText(getApplicationContext(), "near", Toast.LENGTH_SHORT).show();
+                    imagenFoco.setImageResource(R.mipmap.light_on);
+                } else {
+                    //far
+                    imagenFoco.setImageResource(R.mipmap.light_off);
+                    Toast.makeText(getApplicationContext(), "far", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
