4eb3f0da1d5e26045e7ec520abf9fbba96ae81bc
Joaquín Oria <joaquin.ariel.o@gmail.com>
2019-11-18 16:23:48 -0300

    Se agregó el código para detectar el shaking en la activity de dance.

--- app/src/main/java/com/android/brillame/DiscoModeActivity.java
+++ app/src/main/java/com/android/brillame/DiscoModeActivity.java
@@ -2,16 +2,36 @@ package com.android.brillame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DiscoModeActivity extends AppCompatActivity {
-    Singleton singleton;
+    private Singleton singleton;
+    private SensorManager sm;
+
+    private float aceleracionValor;
+    private float ultimoAceleracionValor;
+    private float shake;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disco_mode);
 
         singleton = singleton.getInstance();
+
+        sm =(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sm.registerListener(sensorListener, sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+        aceleracionValor = SensorManager.GRAVITY_EARTH;
+        ultimoAceleracionValor = SensorManager.GRAVITY_EARTH;
+        shake = 0.00F;
     }
 
     @Override
@@ -21,4 +41,30 @@ public class DiscoModeActivity extends AppCompatActivity {
 
 
     }
+
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float x =  sensorEvent.values[0];
+            float y =  sensorEvent.values[1];
+            float z =  sensorEvent.values[2];
+
+            ultimoAceleracionValor = aceleracionValor;
+            aceleracionValor = (float) Math.sqrt((double)(x*x+y*y+z*z));
+            float delta = aceleracionValor - ultimoAceleracionValor;
+            shake = shake * 0.9F + delta;
+
+            Log.i("SHAKE", String.valueOf(shake));
+            if(shake > 5){
+                singleton.showToast("Me estás sacudiendo!!", getApplicationContext());
+
+                /*Acá debería ir el código de enviar el comando dance por bluetooth*/
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
