51f8e878a531a7adf375885a81895b936fa61709
Joaquín Oria <joaquin.ariel.o@gmail.com>
2019-11-18 15:30:38 -0300

    Se agregó gif y mensaje a la activity de disco mode.

--- app/src/main/java/com/android/brillame/DiscoModeActivity.java
+++ app/src/main/java/com/android/brillame/DiscoModeActivity.java
@@ -5,10 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DiscoModeActivity extends AppCompatActivity {
-
+    Singleton singleton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disco_mode);
+
+        singleton = singleton.getInstance();
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        singleton.showToast("Sacude el teléfono para activar el modo disco!!", this);
+
+
     }
 }
